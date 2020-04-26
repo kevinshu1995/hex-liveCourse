5be1a0684d09c860dc63224dcf8e6df6d776d944
KevinHWS <kevinshu1995@gmail.com>
2020-04-26 20:09:05 +0800

    header + landing page

---
+++ week3/public/js/all.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.bar').on('click', function () {
+    $('.menu').toggleClass('js--menu-show')
+  })
+  let windowWidth = $(window).width();
+  $(window).resize(function () {
+    if (!$('.bar').is(':visible') && !$('.menu').css('js--menu-show')) {
+      $('.menu').removeClass('js--menu-show')
+    }
+  })
+})
