0662a850fd5eadd37fb0506c358901308f75a2f6
Kevin Hsu <kevin_hsu@joy.com.tw>
2020-05-27 17:34:31 +0800

    assignment done, admin 剩style

--- week5/app/assets/js/all.js
+++ week5/app/assets/js/all.js
@@ -1,5 +1,18 @@
-console.log('Hello!');
+$(function(){
+    addNavActive()
+})
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+function addNavActive(){
+    let pathname = window.location.pathname;
+    console.log(pathname)
+    switch (pathname) {
+        case '/index.html':
+            $('#sideBar-assignment').addClass('active');
+            console.log(pathname , '/index.html',$('#sideBar-assignment').attr('class'))
+            break;
+        case '/admin.html':
+            $('#sideBar-admin').addClass('active');
+            console.log(pathname , '/admin.html')
+            break;
+    }
+}
