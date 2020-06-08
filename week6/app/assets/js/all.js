55543e12bcaa4a4f25b63b232450b8088b3520bb
Kevin Hsu <kevin_hsu@joy.com.tw>
2020-06-08 17:33:34 +0800

    detail & checkout-info done

--- week6/app/assets/js/all.js
+++ week6/app/assets/js/all.js
@@ -3,4 +3,11 @@ $(function(){
   $('.dropdown-toggle').on('click',function(){
     $(this).find('.js-aniToggle').toggleClass('rotate-180')
   })
+  //checkoutPage
+  $('#toggleCardBtn').on('click',function(){
+    $(this).find('.material-icons').toggleClass('rotate-180')
+  })
+  $(window).resize(function(){
+    if($('#toggleCard').is(':hidden')) $('#toggleCardBtn').trigger('click')
+  })
 });
